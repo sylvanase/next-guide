055e6095c7ffa77c8f62294b892038ce8a74642d
sylvanas <jzm362466464@126.com>
2021-01-16 22:00:08 +0800

    init pro

---
+++ pages/list.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: sylvanas
+ * @Date: 2021-01-16 21:58:59
+ * @LastEditors: sylvanas
+ * @LastEditTime: 2021-01-16 21:59:23
+ * @Description:
+ */
+export default function List() {
+	return <div>List page</div>
+}
